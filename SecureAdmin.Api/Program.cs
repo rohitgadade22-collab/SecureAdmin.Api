0d86ef379b2bfa7fb4487a895f6780335157dfe7
Rohit Gadade <rohitgadade22@gmail.com>
2026-02-10 22:39:33 +0530

    Revert Auto DB Migration

--- SecureAdmin.Api/Program.cs
+++ SecureAdmin.Api/Program.cs
@@ -41,11 +41,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//    db.Database.Migrate();
     
-}
+//}
 
 app.Run();
